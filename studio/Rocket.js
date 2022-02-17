cca58ce88612ba79c172e97a905fdf8e774bf1e1
Angela Mitchell <anglarenmit@gmail.com>
2022-02-16 22:48:54 -0600

    complete studio

---
+++ studio/Rocket.js
@@ -0,0 +1,50 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var sum = 0;
+        // for loop
+        for (var i = 0; i < items.length; i++) {
+            sum += items[i].massKg;
+        }
+        return sum;
+        // forEach approach
+        // items.forEach((item: Payload) => {
+        //   sum += item.massKg;
+        // });
+        // return sum;
+        // reduce approach
+        // return items.reduce(function (a, b): number {
+        //   return a + b.massKg;
+        // }, 0);
+    };
+    Rocket.prototype.currentMassKg = function () {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    };
+    Rocket.prototype.canAdd = function (item) {
+        return this.currentMassKg() + item.massKg <= this.totalCapacityKg;
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        return false;
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        return false;
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
---
+++ studio/Rocket.ts
@@ -0,0 +1,61 @@
+import { Astronaut } from './Astronaut';
+import { Cargo } from './Cargo';
+import { Payload } from './Payload';
+
+export class Rocket {
+  // properties and methods
+  name: string;
+  totalCapacityKg: number;
+  cargoItems: Cargo[] = [];
+  astronauts: Astronaut[] = [];
+
+  constructor(name: string, totalCapacityKg: number) {
+    this.name = name;
+    this.totalCapacityKg = totalCapacityKg;
+  }
+
+  sumMass(items: Payload[]): number {
+    let sum = 0;
+
+    // for loop
+    for (let i = 0; i < items.length; i++) {
+      sum += items[i].massKg;
+    }
+    return sum;
+
+    // forEach approach
+    // items.forEach((item: Payload) => {
+    //   sum += item.massKg;
+    // });
+    // return sum;
+
+    // reduce approach
+    // return items.reduce(function (a, b): number {
+    //   return a + b.massKg;
+    // }, 0);
+  }
+
+  currentMassKg(): number {
+    return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+  }
+
+  canAdd(item: Payload): boolean {
+    return this.currentMassKg() + item.massKg <= this.totalCapacityKg;
+  }
+
+  addCargo(cargo: Cargo): boolean {
+    if (this.canAdd(cargo)) {
+      this.cargoItems.push(cargo);
+      return true;
+    }
+    return false;
+  }
+
+  addAstronaut(astronaut: Astronaut): boolean {
+    if (this.canAdd(astronaut)) {
+      this.astronauts.push(astronaut);
+      return true;
+    }
+    return false;
+  }
+}
